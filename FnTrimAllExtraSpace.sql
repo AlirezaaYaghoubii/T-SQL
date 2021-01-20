2de8af98beacd44535c38336451dddc6b42b5a64
Alireza Yaghoubi <61379582+AlirezaaYaghoubii@users.noreply.github.com>
2021-01-20 17:15:18 +0330

    Update FnTrimAllExtraSpace.sql

--- FnTrimAllExtraSpace.sql
+++ FnTrimAllExtraSpace.sql
@@ -4,7 +4,11 @@ Returns Nvarchar(Max)
 As 
 Begin 
 
-Set @Data=Ltrim(Rtrim(@Data))
+Set @Data=LTRIM(RTRIM(@Data))
+Set @Data=REPLACE(@Data,CHAR(10),'')
+Set @Data=REPLACE(@Data,CHAR(13),'')
+Set @Data=REPLACE(@Data,NCHAR(1603),NCHAR(1705))
+Set @Data=REPLACE(@Data,NCHAR(1610),NCHAR(1740))
 
 While (CHARINDEX('  ',@Data)<>0)
 Set @Data=REPLACE(@Data,'  ',' ') 
